afd2633da880e98f192070985154dfc09a37dbf9
Atharva <atharva.tathe55@gmail.com>
2021-03-27 13:27:38 +0530

    some changes are done in local

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("HelloWorld");
 		System.out.println("Namaste India");
-		System.out.println("some changes are done in this");
+		System.out.println("some changes are done in Local master");
 	}
 }
